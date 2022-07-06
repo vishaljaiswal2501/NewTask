8815756c2d3e00caccded87e8a30de73f8f1965a
rohti891276 <prohit.kumar72@gmail.com>
2022-07-06 12:08:14 +0530

    starting of project-3

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const route = require('./routes/route.js');
+const { default: mongoose } = require('mongoose');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb+srv://VishalJaiswal:vishalashu@newcluster.9n9kfap.mongodb.net/group60Database?retryWrites=true&w=majority", {
+    useNewUrlParser: true
+})
+.then( () => console.log("MongoDb is connected"))
+.catch ( err => console.log(err) )
+
+
+app.use('/', route);
+app.use(cors())
+app.use(multer().any());
+
+app.all('/**', (req, res) => {
+  res.status(404).send({ status: false, message: 'Page Not Found!' });
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Express app running on port ' + (process.env.PORT || 3000));
+});
